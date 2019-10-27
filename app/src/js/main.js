d84b79fb07b48eeaf0908d69550a199eb2811eeb
David Rivero <david.rivero.contractor@bbva.com>
2019-10-27 19:15:37 -0300

    Adjust sources, measures on responsive and alignments

---
+++ app/src/js/main.js
@@ -0,0 +1,9 @@
+(function() {
+  /**
+   * Use for mail service:
+   * - https://mailthis.to/
+   * - https://formspree.io/
+   * - https://formsubmit.co/
+   * - https://formspark.io/
+   */
+})();
